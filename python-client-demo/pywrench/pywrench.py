c7ec478cef4d7dffa771a57ad680ee1557fcd33d
Henri Casanova <henric@hawaii.edu>
2021-09-02 18:35:47 -1000

    Code cleanup

---
+++ python-client-demo/pywrench/pywrench.py
@@ -0,0 +1,116 @@
+import sys
+import requests
+import json
+
+
+class WRENCHException(Exception):
+    def __init__(self, message):
+        self.message = message
+        super().__init__(self.message)
+
+    pass
+
+
+def kill_daemon():
+    try:
+        requests.post("http://localhost:8101/api/stop")
+    except requests.exceptions.ConnectionError:
+        pass
+    return
+
+
+def wait_for_next_event():
+    try:
+        r = requests.get("http://localhost:8101/api/waitForNextSimulationEvent")
+    except requests.exceptions.ConnectionError:
+        raise WRENCHException("Cannot connect to WRENCH daemon");
+
+    response = r.json()
+    return response
+
+
+def submit_standard_job(job_name, cs_name):
+    submission_spec = {"job_name": job_name, "service_name": cs_name}
+    try:
+        r = requests.post("http://localhost:8101/api/submitStandardJob", data=json.dumps(submission_spec))
+    except requests.exceptions.ConnectionError:
+        raise WRENCHException("Cannot connect to WRENCH daemon");
+
+    response = r.json()
+    if response["success"]:
+        return
+    else:
+        raise WRENCHException(response["failure_cause"])
+
+
+def get_simulation_events():
+    try:
+        r = requests.get("http://localhost:8101/api/getSimulationEvents")
+    except requests.exceptions.ConnectionError:
+        raise WRENCHException("Cannot connect to WRENCH daemon");
+
+    print("R.TEXT = " + str(r.text))
+    response = r.json()
+    return response
+
+
+def create_standard_job(task_name, task_flops, min_num_cores, max_num_cores):
+    task_spec = {"task_name": task_name, "task_flops": task_flops, "min_num_cores": min_num_cores,
+                 "max_num_cores": max_num_cores}
+    try:
+        r = requests.post("http://localhost:8101/api/createStandardJob", data=json.dumps(task_spec))
+    except requests.exceptions.ConnectionError:
+        raise WRENCHException("Cannot connect to WRENCH daemon");
+
+    response = r.json()
+    print(response)
+    if response["success"]:
+        return response["job_id"]
+    else:
+        raise WRENCHException(response["failure_cause"])
+
+
+def sleep(seconds):
+    time_spec = {"increment": seconds}
+    try:
+        requests.post("http://localhost:8101/api/addTime", data=json.dumps(time_spec))
+    except requests.exceptions.ConnectionError:
+        raise WRENCHException("Cannot connect to WRENCH daemon");
+
+    return
+
+
+def get_simulated_time():
+    try:
+        r = requests.get("http://localhost:8101/api/getTime")
+    except requests.exceptions.ConnectionError:
+        raise WRENCHException("Cannot connect to WRENCH daemon");
+
+    response = r.json()
+    return response["time"]
+
+
+def create_bare_metal_compute_service(hostname):
+    service_spec = {"service_type": "compute_baremetal", "head_host": hostname}
+    try:
+        r = requests.post("http://localhost:8101/api/addService", data=json.dumps(service_spec))
+    except requests.exceptions.ConnectionError:
+        raise WRENCHException("Cannot connect to WRENCH daemon");
+
+    response = r.json()
+
+    if response["success"]:
+        return response["service_name"]
+    else:
+        raise WRENCHException(response["failure_cause"])
+
+
+def get_all_hostnames():
+    try:
+        r = requests.get("http://localhost:8101/api/getAllHostnames")
+    except requests.exceptions.ConnectionError:
+        raise WRENCHException("Cannot connect to WRENCH daemon");
+
+    response = r.json()
+
+    return response["hostnames"]
---
+++ python-client-demo/simulator.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3  
+from pywrench import pywrench
+
+if __name__ == "__main__":
+
+    print("New simulation, time is " + str(pywrench.get_simulated_time()))
+
+    hosts = pywrench.get_all_hostnames()
+    print("Hosts in the platform are: " + str(hosts))
+
+    print("Creating a bare-metal compute service on ComputeHost...")
+    cs_name = pywrench.create_bare_metal_compute_service("ComputeHost")
+    print("Created service has name " + cs_name)
+
+    print("Sleeping for 10 seconds...")
+    pywrench.sleep(10)
+
+    print("Time now is " + str(pywrench.get_simulated_time()))
+
+    print("Creating a standard job with a single 100.0 flop task")
+    job_name = pywrench.create_standard_job("some_task", 100.0, 1, 1)
+    print("Created standard job has name " + job_name)
+
+    print("Submitting the standard job to the compute service...")
+    pywrench.submit_standard_job(job_name, cs_name)
+    print("Job submitted!")
+
+    print("Sleeping for 1000 seconds...")
+    pywrench.sleep(1000)
+
+    print("Time now is " + str(pywrench.get_simulated_time()))
+
+    print("Getting simulation events that have occurred while I slept...")
+    events = pywrench.get_simulation_events()
+    for event in events["event_queue"]:
+        print("EVENT: " + str(event))
+
+    print("Creating another standard job...")
+    job_name = pywrench.create_standard_job("some_other_task", 100.0, 1, 1)
+    print("Created standard job has name " + job_name)
+
+    print("Submitting the standard job to the compute service...")
+    pywrench.submit_standard_job(job_name, cs_name)
+    print("Job submitted!")
+
+    print("Synchronously waiting for the next simulation event...")
+    event = pywrench.wait_for_next_event()
+    print("EVENT: " + str(event))
+
+    print("Time is " + str(pywrench.get_simulated_time()))
+
+    print("Shutting down the simulation daemon")
+    pywrench.kill_daemon()
+
