8547f80bee689e973153b395ed3ca3626b37c07f
Henri Casanova <henric@hawaii.edu>
2021-09-04 22:37:21 -1000

    typo--

--- python-client-demo/pywrench/pywrench.py
+++ python-client-demo/pywrench/pywrench.py
@@ -4,7 +4,7 @@ from pywrench.exception import WRENCHException
 
 def start_simulation(platform_file_path, controller_hostname, daemon_host="localhost", daemon_port=8101):
     """
-    Start a bew simulation
+    Start a new simulation
     :param platform_file_path: path of a file that contains the simulated platform's description in XML
     :param controller_hostname: the name of the (simulated) host in the platform on which the simulation concroller will run
     :param daemon_host: the name of the host on which the WRENCH daemon is running
