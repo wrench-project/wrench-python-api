d03fff9b13c970ea969d846b817d2085601731ca
Henri Casanova <henric@hawaii.edu>
2024-09-11 00:51:34 -1000

    Attempting a GitHub Action to run tests/examples

---
+++ example/run_all_examples.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+python3 compound_job_simulator/simulator.py
+python3 json_workflow_simulator/simulator.py
+python3 simple_simulator/simulator.py
---
+++ tests/run_all_tests.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+python3 bare_metal_compute_service_test.py
+python3 batch_compute_service_test.py
+python3 cloud_compute_service_test.py
+python3 workflow_job_task_test.py
+python3 workflow_simulator_test.py
