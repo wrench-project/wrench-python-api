6057c984560641c42009a9ba69836071347e3893
Henri Casanova <henric@hawaii.edu>
2024-09-11 11:07:00 -1000

    breaking a test on purpose to validate CI

--- tests/bare_metal_compute_service_test.py
+++ tests/bare_metal_compute_service_test.py
@@ -20,6 +20,8 @@ if __name__ == "__main__":
         simulation = wrench.Simulation()
         simulation.start(platform_file_path, "ControllerHost")
 
+        raise Exception("TEST")
+
         print(f"New simulation, time is {simulation.get_simulated_time()}")
         hosts = simulation.get_all_hostnames()
         print(f"Hosts in the platform are: {hosts}")
