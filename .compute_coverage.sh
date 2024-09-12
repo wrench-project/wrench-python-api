76cdc60bd45db204dff24f727ab5d2542fb95f8d
Henri Casanova <henric@hawaii.edu>
2024-09-11 21:40:09 -1000

    debugged workflow

--- .compute_coverage.sh
+++ .compute_coverage.sh
@@ -7,18 +7,16 @@ first_file_executed=false
 ls -la /home/wrench/
 
 for file in tests/*.py; do
-    echo "FILE: $file"
     if [ -f "$file" ]; then
     	if [ "$first_file_executed" = false ]; then
-        	python3 -m coverage run --data-file=/tmp/.coverage "$file"
+        	python3 -m coverage run --data-file=/tmp/.coverage "$file" 1> /dev/null
 		first_file_executed=true
 	else
-        	python3 -m coverage run --data-file=/tmp/.coverage -a "$file"
+        	python3 -m coverage run --data-file=/tmp/.coverage -a "$file" 1> /dev/null
 	fi
 
     fi
 done
 
-echo "COMPUTING COVERAGE REPORT"
 python3 -m coverage report --data-file=/tmp/.coverage | grep TOTAL | sed "s/.* //"
 echo "(To see full report: python3 -m coverage html --data-file=/tmp/.coverage )" >&2
