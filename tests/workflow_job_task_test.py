11ec71426c80294b8f546371aebc61e9ee4588df
Henri Casanova <henric@hawaii.edu>
2024-09-13 11:51:50 -1000

    Better testing

--- tests/workflow_job_task_test.py
+++ tests/workflow_job_task_test.py
@@ -15,68 +15,54 @@ import wrench
 
 if __name__ == "__main__":
 
+    current_dir = pathlib.Path(__file__).parent.resolve()
+    platform_file_path = pathlib.Path(current_dir / "sample_platform.xml")
+
+    simulation = wrench.Simulation()
     try:
-        current_dir = pathlib.Path(__file__).parent.resolve()
-        platform_file_path = pathlib.Path(current_dir / "sample_platform.xml")
-
-        simulation = wrench.Simulation()
         simulation.start(platform_file_path, "ControllerHost")
-        workflow = simulation.create_workflow()
-        workflow.get_name()
-        str(workflow)
-        repr(workflow)
-
-        print("Adding a a 1kB file to the simulation...")
-        file1 = simulation.add_file("file1", 1024)
-        print(f"Created file {file1}")
-        print("Adding another 1kB file to the workflow...")
-        file2 = simulation.add_file("file2", 2048)
-        print(f"Created file {file2}")
-        print("Adding another 1kB file to the workflow...")
-        file3 = simulation.add_file("file3", 10000)
-        print(f"Created file {file3}")
-
-        print(f"The files in the simulation are {simulation.get_all_files()}")
-
-        print("Creating a task")
-        task1 = workflow.add_task("task1", 100.0, 1, 8, 1024)
-        print(f"Just created a task with flops={task1.get_flops()}" +
-              f", min_num_cores={task1.get_min_num_cores()}" +
-              f", max_num_cores={task1.get_max_num_cores()}" +
-              f", memory={task1.get_memory()}")
-
-        task1.add_input_file(file1)
-        print(f"Attached file {file1} as input file to task {task1.get_name()}")
-        print(f"The list of input files for task {task1.get_name()} is: {task1.get_input_files()}")
-        task1.add_output_file(file2)
-        print(f"Attached file {file2} as output file to task {task1.get_name()}")
-        print(f"The list of output files for task {task1.get_name()} is: {task1.get_output_files()}")
-
-        print("Creating another task")
-        task2 = workflow.add_task("task2", 1000.0, 4, 4, 0)
-        print(f"Just created a task with flops={task2.get_flops()}" +
-              f", min_num_cores={task2.get_min_num_cores()}" +
-              f", max_num_cores={task2.get_max_num_cores()}" +
-              f", memory={task2.get_memory()}")
-
-        task2.add_input_file(file2)
-        print(f"Attached file {file2} as input file to task {task2.get_name()}")
-        print(f"The list of input files for task {task2.get_name()} is: {task2.get_input_files()}")
-        task1.add_output_file(file3)
-        print(f"Attached file {file3} as output file to task {task2.get_name()}")
-        print(f"The list of output files for task {task2.get_name()} is: {task2.get_output_files()}")
-
-        print(f"The tasks in the simulation are: {workflow.get_tasks()}")
-
-        print("Creating a standard job with both tasks")
-        job = simulation.create_standard_job([task1, task2], {})
-        print(f"Created standard job has name {job.get_name()}")
-
-        print(f"This job contains the following tasks: {job.get_tasks()}")
-
-        print("Terminating simulation")
-        simulation.terminate()
-
     except wrench.WRENCHException as e:
         sys.stderr.write(f"Error: {e}\n")
         exit(1)
+
+    workflow = simulation.create_workflow()
+    workflow.get_name()
+    str(workflow)
+    repr(workflow)
+
+    file1 = simulation.add_file("file1", 1024)
+    assert file1.get_size() == 1024, "File1 has an incorrect size"
+    file2 = simulation.add_file("file2", 2048)
+    file3 = simulation.add_file("file3", 10000)
+
+    files = simulation.get_all_files()
+    assert files[file1.get_name()] == file1, "File1 should be known to the simulation"
+    assert files[file2.get_name()] == file2, "File2 should be known to the simulation"
+    assert files[file3.get_name()] == file3, "File3 should be known to the simulation"
+
+    task1 = workflow.add_task("task1", 100.0, 1, 8, 1024)
+    str(task1)
+    repr(task1)
+
+    assert task1.get_name() == "task1", "Task1 has an incorrect name"
+    assert task1.get_flops() == 100.0, "Task1 has an incorrect flops"
+    assert task1.get_min_num_cores() == 1, "Task1 has an incorrect min num cores"
+    assert task1.get_max_num_cores() == 8, "Task1 has an incorrect max num cores"
+    assert task1.get_memory() == 1024, "Task1 has an incorrect ram footprint"
+
+    task1.add_input_file(file1)
+    assert file1 in task1.get_input_files(), "File1 should be input to Task1"
+    task1.add_output_file(file2)
+    assert file2 in task1.get_output_files(), "File1 should be output from Task1"
+
+    task2 = workflow.add_task("task2", 1000.0, 4, 4, 0)
+    task2.add_input_file(file2)
+    task1.add_output_file(file3)
+    job = simulation.create_standard_job([task1, task2], {})
+    str(job)
+    repr(job)
+    assert task1 in job.get_tasks(), "Task1 should be in job"
+    assert task2 in job.get_tasks(), "Task2 should be in job"
+
+    simulation.terminate()
+
