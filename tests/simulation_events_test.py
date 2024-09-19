a7cf0fe15b07225c7ca4c5dc32aade699fcaeae0
Henri Casanova <henric@hawaii.edu>
2024-09-18 18:43:37 -1000

    test more explicit

--- tests/simulation_events_test.py
+++ tests/simulation_events_test.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
 
     events = simulation.get_events()
 
-    assert len(events) == 1, f"There should be a single simulation event (instead there are {len(events)}"
+    assert len(events) == 1, "There should be a single simulation event (instead there are " + str(len(events)) + ")"
     assert events[0]["event_type"] == "standard_job_completion", "Event type is unexpected"
 
     simulation.terminate()
