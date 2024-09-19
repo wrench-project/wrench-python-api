25b1f575dfc23cae6a7b484e1f2ac46d4af718d4
Henri Casanova <henric@hawaii.edu>
2024-09-18 19:50:24 -1000

    small fixes

--- examples/compound_job_simulator/simulator.py
+++ examples/compound_job_simulator/simulator.py
@@ -1,3 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2021 The WRENCH Team.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+
 import pathlib
 import wrench
 
